9e27e10ff7e6ac71a161858192fa4f1af510aa31
MarioUlakaevich <ulakaev05@gmail.com>
2020-08-19 00:14:12 +0300

    Second Commit

---
+++ Web Development/JavaScript/Objects/Objects/nested_objects.js
@@ -0,0 +1,42 @@
+let spaceship = {
+  passengers: null,
+  telescope: {
+    yearBuilt: 2018,
+    model: "91031-XLT",
+    focalLength: 2032 
+  },
+  crew: {
+    captain: { 
+      name: 'Sandra', 
+      degree: 'Computer Engineering', 
+      encourageTeam() { console.log('We got this!') },
+     'favorite foods': ['cookies', 'cakes', 'candy', 'spinach'] }
+  },
+  engine: {
+    model: "Nimbus2000"
+  },
+  nanoelectronics: {
+    computer: {
+      terabytes: 100,
+      monitors: "HD"
+    },
+    'back-up': {
+      battery: "Lithium",
+      terabytes: 50
+    }
+  }
+}; 
+
+const capFave = spaceship.crew.captain['favorite foods'][0];
+
+spaceship.passengers = [{
+  name: 'Marat',
+  age: 24
+}, {
+  name: 'Sultan',
+  age: 22
+}];
+
+const firstPassenger = spaceship.passengers[0];
+
+console.log(firstPassenger.name);
